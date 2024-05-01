d20ff8dc1073a6fbe59ab925bca53ab2b700140d
root <root@Felipe>
2024-04-30 21:41:24 -0300

    feat: add stacks.

---
+++ src/datastructures/stacks/Main.java
@@ -0,0 +1,12 @@
+package datastructures.stacks;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack myStack = new Stack(4);
+
+        myStack.getHeight();
+        myStack.getTop();
+
+        myStack.printStack();
+    }
+}
