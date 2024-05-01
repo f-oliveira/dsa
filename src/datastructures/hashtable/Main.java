01952b481d02f48410ea178c84638eeeb81bb9ad
root <root@Felipe>
2024-04-30 21:43:09 -0300

    feat: add hash table.

---
+++ src/datastructures/hashtable/Main.java
@@ -0,0 +1,16 @@
+package datastructures.hashtable;
+
+public class Main {
+    public static void main(String[] args) {
+        HashTable myHashTable = new HashTable();
+
+        myHashTable.set("nails", 100);
+        myHashTable.set("tile", 50);
+        myHashTable.set("lumber", 80);
+
+        myHashTable.set("bolts", 200);
+        myHashTable.set("nails", 140);
+
+        myHashTable.printTable();
+    }
+}
