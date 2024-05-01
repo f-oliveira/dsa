f9c023a9d1f239fae48ab2260f9741dab94d9bbf
root <root@Felipe>
2024-04-30 21:41:02 -0300

    feat: add linkedlist.

---
+++ src/datastructures/linkedlist/Main.java
@@ -0,0 +1,19 @@
+package datastructures.linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        System.out.println(myLinkedList.findMiddleNode().value);
+
+//        myLinkedList.printList();
+
+    }
+}
