5267cd94c42cc9bc7f1c7cae11a740ceedaa2951
Felipe N. Oliveira <felipe.noliveira19@gmail.com>
2024-05-05 21:04:27 -0300

    fix: array length.

--- src/sorts/bubblesort/BubbleSort.java
+++ src/sorts/bubblesort/BubbleSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class BubbleSort {
     
     public static void bubbleSort(int[] array) {
-        for (int i = array.length; i > 0; i--) {
+        for (int i = array.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (array[j] > array[j+1]) {
                     int temp = array[j];
