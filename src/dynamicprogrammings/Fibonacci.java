a6cfbf34ea7c23e3af5c68fe70233e4858e66e9f
Felipe N. Oliveira <felipe.noliveira19@gmail.com>
2024-05-08 21:13:20 -0300

    feat: add dynamic programing.

---
+++ src/dynamicprogrammings/Fibonacci.java
@@ -0,0 +1,14 @@
+package dynamicprogrammings;
+
+public class Fibonacci {
+
+    public static int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fib(n - 1);
+    }
+    public static void main(String[] args) {
+        fib(5);
+    }
+}
